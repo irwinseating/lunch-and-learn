5d892c254d7807b31f9738f88a1c67b9d87cbc2c
Kevin Kayser <kevin.f.kayser@gmail.com>
2021-07-29 15:15:29 -0400

    some cleanup

--- July 29, 2021/js/pepperDataMain.js
+++ July 29, 2021/js/pepperDataMain.js
@@ -2,59 +2,69 @@ function setJson(data) {
     document.getElementById("json").innerHTML = JSON.stringify(data, null, 2);
 }
 
-function fetchData(url) {
-    return fetch(url);
-}
-
-function getPepperNameAndHeat(data) {
+function getPepperMetaData(data) {
     return data.map(function (m) {
-        return { "name": m.name, "heat": m.heat };
+
+        // returns the max and min heat of the data
+        var determineMinJrS = (p) => p.min_jrp > 0 ? p.min_jrp : p.min_shu;
+        var determineMaxJrS = (p) => p.max_jrp > 0 ? p.max_jrp : p.max_shu;
+        return { "name": m.name, "heat": m.heat, minHeat: determineMinJrS(m) ?? "?", maxHeat: determineMaxJrS(m) ?? "?" };
     });
 }
 
-function parsePeppersFromDataJson(data) {
-    return getPepperNameAndHeat(data.peppers);
+function displayHeatRange(pepper) {
+    var { minHeat, maxHeat } = pepper;
+    return `(${minHeat}-${maxHeat})`;
 }
 
-function createLengend(pepperData) {
+// This isn't complete, but it sure does get a distinct set of heat types
+function createLegend(pepperData) {
+
+    var distinctHeats = pepperData.map(m => m.heat).filter((value, index, self) => self.indexOf(value) === index);
+    return distinctHeats.map(m => { return { "name": m, "symbol": displayHeatSymbol({ "heat": m }) } });
 }
+
+// returns a friendly display of the heat instead of just text.
 function displayHeatSymbol(pepper) {
-    var { heat } = pepper;
+    var { heat } = pepper; // variable descructuring
+    /*
+    The same thing as the following:
+    var heat = pepper.heat;
+    or 
+    var heat = pepper["heat"]; 
 
-    let heatOptions = { mild: "mild", medium: "medium", hot: "hot" };
+    */
+
     switch (heat) {
         case null:
             return "";
-            break;
         case "mild":
             return '🆒';
-            break;
         case "hot":
             return '🥵';
-            break;
         case "medium":
             return '🔉';
-            break; case "nuclear":
+        case "nuclear":
             return '☠';
-            break;
+
         default: return heat;
     }
 
 }
 
+// createds joins the list of items and throws them into pepperWrapper element
 function putDataOnPage(data) {
     let wrapper = document.getElementById("pepperFields");
 
-    let pepperListItems = data.map(m => `<div class="card"><div class="flex flex-row justify-between m-5"><div>${m.name}</div><div> ${displayHeatSymbol(m)}</div></div></div>`).join('');
+    let pepperListItems = data.map(m => `<div class="card"><div class="flex flex-row justify-between m-5"><div>${m.name} ${displayHeatRange(m)}</div><div> ${displayHeatSymbol(m)}</div></div></div>`).join('');
     wrapper.innerHTML = `<div class="grid grid-cols-2 gap-5 pepper-wrapper">${pepperListItems}</div>`;
 }
 
 (function () {
-    var url = "../pepperData.json"
+    var url = "./data/peppers.json";
 
-    fetchData(url)
+    fetch(url)
         .then(r => r.json())
-        .then(r => parsePeppersFromDataJson(r))
-        .then(r => getPepperNameAndHeat(r))
-        .then(r => { putDataOnPage(r); setJson(r) });
+        .then(r => getPepperMetaData(r.peppers))
+        .then(r => { putDataOnPage(r); setJson(r); createLegend(r); });
 })();
