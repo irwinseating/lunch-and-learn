b93d1597c8b26aca520d3a12bcf1f924ede1cef3
Kevin Kayser <kevin.f.kayser@gmail.com>
2021-07-29 15:26:20 -0400

    Using Const, not var

--- July 29, 2021/js/pepperDataMain.js
+++ July 29, 2021/js/pepperDataMain.js
@@ -13,20 +13,19 @@ function getPepperMetaData(data) {
 }
 
 function displayHeatRange(pepper) {
-    var { minHeat, maxHeat } = pepper;
+    const { minHeat, maxHeat } = pepper;
     return `(${minHeat}-${maxHeat})`;
 }
 
 // This isn't complete, but it sure does get a distinct set of heat types
 function createLegend(pepperData) {
-
-    var distinctHeats = pepperData.map(m => m.heat).filter((value, index, self) => self.indexOf(value) === index);
+    const distinctHeats = pepperData.map(m => m.heat).filter((value, index, self) => self.indexOf(value) === index);
     return distinctHeats.map(m => { return { "name": m, "symbol": displayHeatSymbol({ "heat": m }) } });
 }
 
 // returns a friendly display of the heat instead of just text.
 function displayHeatSymbol(pepper) {
-    var { heat } = pepper; // variable descructuring
+    const { heat } = pepper; // variable descructuring
     /*
     The same thing as the following:
     var heat = pepper.heat;
@@ -54,14 +53,14 @@ function displayHeatSymbol(pepper) {
 
 // createds joins the list of items and throws them into pepperWrapper element
 function putDataOnPage(data) {
-    let wrapper = document.getElementById("pepperFields");
+    const wrapper = document.getElementById("pepperFields");
 
     let pepperListItems = data.map(m => `<div class="card"><div class="flex flex-row justify-between m-5"><div>${m.name} ${displayHeatRange(m)}</div><div> ${displayHeatSymbol(m)}</div></div></div>`).join('');
     wrapper.innerHTML = `<div class="grid grid-cols-2 gap-5 pepper-wrapper">${pepperListItems}</div>`;
 }
 
 (function () {
-    var url = "./data/peppers.json";
+    let url = "./data/peppers.json";
 
     fetch(url)
         .then(r => r.json())
