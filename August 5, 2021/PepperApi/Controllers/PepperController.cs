a0262bbe43ad2ee14462cb9a9767bc961615bd3d
Kevin Kayser <kevin.f.kayser@gmail.com>
2021-08-04 14:51:24 -0400

    Adding a pepper API in C# for taking steps toward tokens

---
+++ August 5, 2021/PepperApi/Controllers/PepperController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PepperApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PepperController : ControllerBase
+    {
+        private readonly ILogger<PepperController> _logger;
+
+        public PepperController(ILogger<PepperController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 3600)]
+        public async Task<IActionResult> Get()
+        {
+            using (StreamReader r = new StreamReader("data/peppers.json"))
+            {
+                var json = r.ReadToEnd();
+                var pepperResponse = JsonConvert.DeserializeObject<PepperResponse>(json);
+                return Ok(pepperResponse.Peppers.Take(100));
+            }
+        }
+    }
+}
